d556f0b3ca2e31aeae57843c0e20eeab0bc8379e
Zeeshan Akram <zeeshanakram1704@gmail.com>
2025-05-09 22:41:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import streamlit as st
 import os
 import nltk
-
-nltk_data_dir = os.path.join(os.getcwd(), 'nltk_data')
-os.makedirs(nltk_data_dir, exist_ok=True)
-
-nltk.download('punkt', download_dir=nltk_data_dir)
-nltk.download('stopwords', download_dir=nltk_data_dir)
-nltk.data.path.append(nltk_data_dir)
+nltk.download("punkt")
+nltk.download("stopwords")
 import re
 import string
 import numpy as np
