e64f6b042071207680338e3a12908f34fa732d94
Zeeshan Akram <zeeshanakram1704@gmail.com>
2025-05-09 22:38:09 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ os.makedirs(nltk_data_dir, exist_ok=True)
 
 nltk.download('punkt', download_dir=nltk_data_dir)
 nltk.download('stopwords', download_dir=nltk_data_dir)
-nltk.data.path.append(nltk_data_dir)import re
+nltk.data.path.append(nltk_data_dir)
+import re
 import string
 import numpy as np
 import pandas as pd
