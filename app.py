4948b6eaeb641c808ac38b74608a574aec5c2c43
Zeeshan Akram <zeeshanakram1704@gmail.com>
2025-05-20 13:03:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -92,24 +92,25 @@ if viz_option != "None":
     st.subheader("Visualization")
 
     if viz_option == "WordClouds":
-        st.image("wordcloud_spam.png", caption="Spam WordCloud", use_container_width=True)
-        st.image("wordcloud_ham.png", caption="Ham WordCloud", use_container_width=True)
+        st.image("plots/wordcloud_spam.png", caption="Spam WordCloud", use_container_width=True)
+        st.image("plots/wordcloud_ham.png", caption="Ham WordCloud", use_container_width=True)
 
     elif viz_option == "Most Frequent Words":
-        st.image("spam_ham_top_words.png", caption="Most Frequent Words in Spam and Ham", use_container_width=True)
+        st.image("plots/spam_ham_top_words.png", caption="Most Frequent Words in Spam and Ham", use_container_width=True)
 
     elif viz_option == "Top Bigrams":
-        st.image("spam_ham_top_bigrams.png", caption="Top 10 Bigrams in Spam and Ham", use_container_width=True)
+        st.image("plots/spam_ham_top_bigrams.png", caption="Top 10 Bigrams in Spam and Ham", use_container_width=True)
 
     elif viz_option == "Threshold vs Metrics":
-        st.image("thres_vs_metrics_spam_detection.png", caption="Precision, Recall, F1 vs Threshold", use_container_width=True)
+        st.image("plots/thres_vs_metrics_spam_detection.png", caption="Precision, Recall, F1 vs Threshold", use_container_width=True)
 
     elif viz_option == "ROC and PR Curves":
-        st.image("roc_curve_spam_detection.png", caption="ROC Curve", use_container_width=True)
-        st.image("pr_curve_spam_detection.png", caption="Precision-Recall Curve", use_container_width=True)
+        st.image("plots/roc_curve_spam_detection.png", caption="ROC Curve", use_container_width=True)
+        st.image("plots/pr_curve_spam_detection.png", caption="Precision-Recall Curve", use_container_width=True)
 
     elif viz_option == "Top 20 Spam Indicator Words":
-        st.image("top_20_spam_indicators.png", caption="Top 20 Spam Indicator Words from BernoulliNB", use_container_width=True)
+        st.image("plots/top_20_spam_indicators.png", caption="Top 20 Spam Indicator Words from BernoulliNB", use_container_width=True)
+
     elif viz_option == "Confusion Matrix":
-        st.image("confusion_matrix.png", caption="Confusion Matrix", use_container_width=True)
+        st.image("plots/confusion_matrix.png", caption="Confusion Matrix", use_container_width=True)
 st.markdown("<br><br><center><i>Developed by Zeeshan Akram</i></center>", unsafe_allow_html=True)
